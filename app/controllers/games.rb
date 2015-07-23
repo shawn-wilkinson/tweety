670298ab3a9d6a6cead995e3ff4d9b9901132645
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-07-23 17:31:48 -0500

    fix bugs, seed database

---
+++ app/controllers/games.rb
@@ -0,0 +1,9 @@
+get '/games/new' do
+
+  #The user decides to play a game by clicking on a deck
+#A new intance of the game class is created with that deck as it's deck
+#after the game is initilized, new_game.populate_questions is run.
+#this method scrapes the 'tweeters' of that deck and creates 20(or whatever) suitable question instances
+#
+end
+
